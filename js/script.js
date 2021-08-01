065efbc36f3d4d69bd85cb3bfc4ab131084df1b7
anarstah <anarstah@gmail.com>
2021-08-01 20:06:01 +0300

    first version of personalMoviesDB

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 "use strict";
 
+const numberOfFilms = +prompt('how many films have you watched?');
+let k = 0;
+
+
+let personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privacy: false
+};
+
+for (let i = 0; i <= k; i++) {
+    let a = prompt('Yours last film?');
+    let b = +prompt('Rating?');
+    personalMoviesDB.movies[a] = b;
+    let another = confirm('another movie?');
+    if (another === true) {
+        k++;
+    }
+}
+
+console.log(personalMoviesDB);
+
+
 
