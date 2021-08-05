1db863180c7a4bf911a3e5f486bcc26f78cb3df0
anarstah <anarstah@gmail.com>
2021-08-05 21:46:25 +0300

    fix code with function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt('how many films have you watched?');
-let k = 0;
-
+let numberOfFilms;
 
 let personalMoviesDB = {
     count: numberOfFilms,
@@ -12,31 +10,65 @@ let personalMoviesDB = {
     privacy: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Yours last film?');
-    let b = +prompt('Rating?');
+function start() {
+    numberOfFilms = +prompt('how many films have you watched?');
 
-    if ((a && b) != null && (a && b) != '' && a.length < 50) {
-        console.log('done');
-        personalMoviesDB.movies[a] = b;
-    } else {
-        console.log('error');
-        i--;
+    while (numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('how many films have you watched?');
     }
-
-    console.log(personalMoviesDB);
-
 }
 
-if (personalMoviesDB.count <= 10) {
-    alert('slabak');
-} else if (personalMoviesDB.count <= 30) {
-    alert('norm');
-} else if (personalMoviesDB.count > 30) {
-    alert('krasava');
-} else {
-    alert('error');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Yours last film?');
+        let b = +prompt('Rating?');
+
+        if ((a && b) != null && (a && b) != '' && a.length < 50) {
+            console.log('done');
+            personalMoviesDB.movies[a] = b;
+        } else {
+            console.log('error');
+            i--;
+        }
+
+        console.log(personalMoviesDB);
+    }
 }
+
+function detectPersonalLevel() {
+    if (personalMoviesDB.count <= 10) {
+        alert('slabak');
+    } else if (personalMoviesDB.count <= 30) {
+        alert('norm');
+    } else if (personalMoviesDB.count > 30) {
+        alert('krasava');
+    } else {
+        alert('error');
+    }
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMoviesDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i <3; i++) {
+        personalMoviesDB.genres[i] = prompt(`What is your favorite genre number ${i + 1}?`);
+    }
+}
+
+
+
+start();
+
+rememberMyFilms();
+
+writeYourGenres();
+
+showMyDB(personalMoviesDB.privacy);
+
 
 
 
